7a56cd6b42d9604e503cb5f14d3859d2a998a80f
Pedro Vinicius <silvapv7@gmail.com>
2022-03-19 14:37:33 -0400

    Configurações css/scss

--- ignite-aulas/reactjs/01-github-explorer/webpack.config.js
+++ ignite-aulas/reactjs/01-github-explorer/webpack.config.js
@@ -28,9 +28,14 @@ module.exports = {
     module: {
         rules: [
             {
-              test: /\.jsx$/, 
-              exclude: /node_modules/,
-              use: 'babel-loader',
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: 'babel-loader' 
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader','sass-loader'],
             }
         ],
     }
