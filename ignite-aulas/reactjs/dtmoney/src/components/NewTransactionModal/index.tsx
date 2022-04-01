9e53eb9df717d4be02c5039c6dbda9963a981774
Pedro Vinicius <silvapv7@gmail.com>
2022-04-01 17:04:00 -0400

    Funcionamento dos Botoes de entrada e saida

--- ignite-aulas/reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
+++ ignite-aulas/reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,10 @@
+import { useState } from "react";
 import Modal from "react-modal";
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 
-import { Container, TransactionsTypeContainer } from "./styles";
+import { Container, TransactionsTypeContainer, RadioBox } from "./styles";
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -12,6 +13,8 @@ interface NewTransactionModalProps {
 
 
 export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModalProps){
+    const [type, setType] = useState('deposit');
+
     return(
         <Modal 
             isOpen={isOpen}
@@ -40,19 +43,23 @@ export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModal
                 />
 
                 <TransactionsTypeContainer>
-                    <button 
+                    <RadioBox 
                         type="button"
+                        onClick={() => {setType('deposit')}}
+                        isActive={type === 'deposit'}
                     >
                         <img src={incomeImg} alt="Entrada" />
                         <span>Entrada</span>
-                    </button>
+                    </RadioBox>
 
-                    <button 
+                    <RadioBox 
                         type="button"
+                        onClick={() => {setType('withdraw')}}
+                        isActive={type === 'withdraw'}
                     >
                         <img src={outcomeImg} alt="Saída" />
                         <span>Saída</span>
-                    </button>
+                    </RadioBox>
                 </TransactionsTypeContainer>
 
                 <input 
